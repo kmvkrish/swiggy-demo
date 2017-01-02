326f306a7fdfd28b1554bee18b04edd043e7b9e6
Krishnamanoj Varanasi <Krishnamanoj.Varanasi@infor.com>
2017-01-02 17:30:42 +0530

    First commit"

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.set("PORT", 3000);
-app.use(express.static(__dirname));
-
-app.listen(app.get("PORT"), () => {
-	console.log("Server stated on port ", app.get("PORT"));
-});
--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ var app = angular.module('Swiggy', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
 	$routeProvider
 	.when('/restaurants', {
-		templateUrl: 'restaurants.html',
+		templateUrl: 'swiggy-demo/restaurants.html',
 		controller: 'RestaurantsCtrl'
 	})
 	.when('/restaurant/:name', {
-		templateUrl: 'menu.html',
+		templateUrl: 'swiggy-demo/menu.html',
 		controller: 'MenuCtrl'
 	});
 
@@ -18,13 +18,13 @@ app.config(function($routeProvider, $locationProvider){
 
 app.controller('RestaurantsCtrl', function($scope, $http, $window){
 	$window.document.title = "Restaurants";
-	$http.get('swiggy-demo.json').then((response) => {
+	$http.get('swiggy-demo/swiggy-demo.json').then((response) => {
 		$scope.restaurants = response.data.restaurants;
 	});
 });
 
 app.controller('MenuCtrl', function($window, $scope, $http, $routeParams){
-	$http.get('swiggy-demo.json').then((response) => {
+	$http.get('swiggy-demo/swiggy-demo.json').then((response) => {
 		angular.forEach(response.data.restaurants, (restaurant, index) => {
 			if(restaurant.name == $routeParams.name){
 				$scope.restaurant = restaurant;
