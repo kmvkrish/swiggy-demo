2613b7ce0472906e442d34b07a12ae5f7f299b8d
Krishna Manoj Varanasi <krishnamanojvaranasi.kmv@gmail.com>
2017-01-02 08:41:36 +0530

    First commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var app = angular.module('Swiggy', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+	.when('/restaurants', {
+		templateUrl: 'restaurants.html',
+		controller: 'RestaurantsCtrl'
+	})
+	.when('/restaurant/:name', {
+		templateUrl: 'menu.html',
+		controller: 'MenuCtrl'
+	});
+
+	$routeProvider.otherwise({
+		redirectTo: '/restaurants'
+	});
+});
+
+app.controller('RestaurantsCtrl', function($scope, $http, $window){
+	$window.document.title = "Restaurants";
+	$http.get('swiggy-demo.json').then((response) => {
+		$scope.restaurants = response.data.restaurants;
+	});
+});
+
+app.controller('MenuCtrl', function($window, $scope, $http, $routeParams){
+	$http.get('swiggy-demo.json').then((response) => {
+		angular.forEach(response.data.restaurants, (restaurant, index) => {
+			if(restaurant.name == $routeParams.name){
+				$scope.restaurant = restaurant;
+				$window.document.title = $routeParams.name;
+			}
+		});
+	});
+	$scope.removeItemFromCart = function(item){
+		if(item.cart_quantity > 1){
+			item.cart_quantity -= 1;
+		}else{
+			item.cart_quantity = 0;
+		}
+	};
+	$scope.addItemToCart = function(item){
+		item.cart_quantity += 1;
+		console.log($scope.restaurant.items);
+	};
+});
